7f20cf7e1d8f33a9182bac61b3c97cc1c70a58cc
youKisnotKingdom <nyu24mataka@gmail.com>
2024-05-12 23:48:06 +0900

    Setup astro.js

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+import remarkCodeTitles from "remark-code-titles";
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	markdown: { 
+		remarkPlugins: [remarkCodeTitles],
+		extendDefaultPlugins: true 
+	},
+	integrations: [mdx(), sitemap()],
+});
